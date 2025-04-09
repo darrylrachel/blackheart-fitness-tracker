c1675b819c307c90f675c9c8822c4393a151f07f
darrylrachel <racheldarryl@icloud.com>
2025-04-09 16:40:23 -0700

    feat(auth): add logout button to dashboard with Supabase signOut

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,11 +1,23 @@
+import { supabase } from '../utils/supabase';
+import { useNavigate } from 'react-router-dom';
+import Button from '../components/Button';
 import Layout from '../components/Layout';
 
 export default function  DashboardPage() {
+  const navigate = useNavigate();
+
+  async function handleLogout() {
+    await supabase.auth.signout();
+    navigate('/login');
+  }
+
+
   return (
     <Layout>
-      <h1 className='text-3xl font-bold mb-4'>
-        Dashboard
-      </h1>
+      <div className='min-h-screen flex flex-col justify-center item-center bg-darkBlue text-lightGray px-4'>
+        <h1 className='text-3xl font-bold mb-6'>Welcome to your Dashboard</h1>
+        <Button variant='danger' onClick={handleLogout}>Log Out</Button>
+      </div>
     </Layout>
   );
 }
